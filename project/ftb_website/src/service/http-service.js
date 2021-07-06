4c543d59739480ae84955a2bb17cc7a49f6cc486
Francesco Panico <francescopanico@MacBook-Pro-di-Francesco.local>
2021-07-06 23:12:17 +0200

    [FTB] set loginEndpoint

--- project/ftb_website/src/service/http-service.js
+++ project/ftb_website/src/service/http-service.js
@@ -1,14 +1,17 @@
 import 'whatwg-fetch';
 
-const dbEndpoint = 'https://ftbstudiodb-0a35.restdb.io/rest/ftb-team?q={}';
+const dbEndpoint = 'https://ftbstudiodb-0a35.restdb.io';
 const myCorsApiKey = '60e213b5c60cf31676a22ad0';
 
 class HttpService {
 
     dbLogin = () =>{
+
+        var loginEndpoint = dbEndpoint + '/https://ftbstudiodb-0a35.restdb.io/rest/ftb-user?q={}';
+
         var promise = new Promise((resolve, reject) => {
 
-            fetch(dbEndpoint, {
+            fetch(loginEndpoint, {
                 method: 'GET',
                 mode: 'cors',
                 headers:{
